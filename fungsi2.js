e8f6f4524c1d812ceec48fe2d4604c7def5ee886
ramadoni16 <donicyberit@gmail.com>
2024-05-05 13:01:36 +0700

    testing ajaa

---
+++ fungsi2.js
@@ -0,0 +1,2 @@
+const deduct = (a, b) => a - b;
+console.log(deduct(5, 4));
--- test3.js
+++
@@ -1,6 +0,0 @@
-console.log("ini data yang akan ditambahkan");
-
-const add = (a, b) => a * b;
-
-//print dulu ini coba
-console.log(add(3, 1));
