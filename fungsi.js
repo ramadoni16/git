d71d5a3def89064191a338a62d054125bb050359
ramadoni16 <donicyberit@gmail.com>
2024-05-05 18:35:27 +0700

    mervisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,15 @@ console.log(add(1, 2));
 const subtrack = (a, b) => a - b;
 console.log(subtrack(9, 4));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(5, 8));
+const multiply_Number = (a, b) => a * b;
+console.log(multiply_Number(5, 8));
 
 const devide = (a, b) => a / b;
 console.log(devide(9, 3));
+
+//merivisi komit komit yang sudah terjadi
+// --checkout;
+
+// --reset;
+
+// --revert;
